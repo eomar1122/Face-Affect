9e2712ace0536cc39e38f4200f07b371b21ffc38
Clark Maxam <clarkmaxam@Clarks-MacBook-Pro.local>
2018-02-04 15:06:34 -0700

    starter pack

---
+++ assets/javascript/main.js
@@ -0,0 +1,11 @@
+//alert("test for js linked up and working")
+
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
