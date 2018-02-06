7fab1c80d216f93faba122db45748cfc68be85d4
Takashi Waseda <cameron@emagispace.com>
2018-02-06 14:12:45 -0700

    made changed to the nav and rounded images

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,3 +19,22 @@ window.onclick = function(event) {
         model2.style.display = "none";
     }
 }
+
+
+
+
+
+var sadArray = [mood,chill];
+
+var happyArray = [mood,pop];
+
+var madArray = [mood,edm];
+
+var shittyDayArray = [mood,workout];
+
+var loveArray = [mood,romance];
+
+var sleepyArray = [mood,sleep];
+
+
+
